90039dd217007b468ec83912f2b8c6e153f64a89
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2016-02-15 00:17:20 +0900

    Improve ParameterUtils

--- 4.x/spring-examples-forum/src/main/java/com/github/tachesimazzoca/spring/examples/forum/util/ParameterUtils.java
+++ 4.x/spring-examples-forum/src/main/java/com/github/tachesimazzoca/spring/examples/forum/util/ParameterUtils.java
@@ -1,21 +1,34 @@
 package com.github.tachesimazzoca.spring.examples.forum.util;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ParameterUtils {
     public static Map<String, Object> params(Object... args) {
-        Map<String, Object> m = new HashMap<String, Object>();
+        return ParameterUtils.<String, Object>map(args);
+    }
+
+    @SafeVarargs
+    public static <T> List<T> list(T... args) {
+        return Arrays.asList(args);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <K, V> Map<K, V> map(Object... args) {
+        Map<K, V> m = new HashMap<K, V>();
         if (args.length % 2 != 0)
             throw new IllegalArgumentException(
                     "The number of args must be even.");
         for (int i = 0; i < args.length; i += 2) {
             if (args[i + 1] != null)
-                m.put((String) args[i], args[i + 1]);
+                m.put((K) args[i], (V) args[i + 1]);
         }
         return m;
     }
 
+    @SafeVarargs
     public static <T> T nullTo(T... args) {
         for (T arg : args) {
             if (arg != null)
